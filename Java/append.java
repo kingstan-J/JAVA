89641e359b6fc1c39b69c1a22190ff54eb47a2c4
kingstan-J <kingstan22082007@gmail.com>
2025-07-09 10:14:11 +0530

    Delete Java directory

--- Java/append.java
+++
@@ -1,9 +0,0 @@
-class append
-{
-    public static void main(String[] args)
-    {
-        String name;
-        name="kingstan";
-        System.out.println("name is:"+name);
-    }
-}
--- Java/print.java
+++
@@ -1,9 +0,0 @@
-class Print
-{
-    public static void main(String[] args)
-    {
-        System.out.println("hello");
-        System.out.print("I am");
-        System.out.println("kingstan");
-    }
-} 
