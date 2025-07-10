f6e4e701d27b4ec5dbd5fb9d6ffc1114cfbec2f1
kingstan-J <kingstan22082007@gmail.com>
2025-07-10 14:35:27 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,14 @@
+package stringApplication;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=100;i<=200;i++)
+		{
+			System.out.println("i="+i);
+		}
+
+	}
+
+}
