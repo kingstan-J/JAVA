36f9d920ece87dce2a7d8b3027d79a42ca21d74c
kingstan-J <kingstan22082007@gmail.com>
2025-07-10 14:34:07 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,18 @@
+package stringApplication;
+
+public class even {
+
+	public static void main(String[] args) {
+		System.out.println("the Even numbers are:");
+		// TODO Auto-generated method stub
+        for(int i=50;i<=100;i++)
+        {
+        	if(i%2==0)
+        	{
+        		System.out.println(i);
+        		
+        	}
+        }
+	}
+
+}
