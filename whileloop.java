8f1ea04a29dffe261df1144000df80ec46c6e1b5
kingstan-J <kingstan22082007@gmail.com>
2025-07-10 14:37:16 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+package stringApplication;
+public class whileloop {
+	public static void main(String[] args)
+	{
+     int i=100;
+     while(i<=200)
+     {
+    	 System.out.println("i="+i);
+    	 i++;
+     }
+	}
+}
